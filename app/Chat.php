729a33ba2e72f0d98c7ac8ffd4e8e9bc7f92f2a9
Gerrit <gerrit.sevilla@gmail.com>
2020-04-09 15:08:41 +0200

    S3 setup local con su test

---
+++ app/Chat.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+
+class Chat extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'messagetype',
+        'messagetext',
+
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+
+    ];
+}
